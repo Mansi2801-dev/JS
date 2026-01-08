85642ccbfb9e45572307d831953b150846f0e0a9
Mansi2801-dev <saxenamansi804@gmail.com>
2026-01-08 15:03:40 +0000

    Basics of js

---
+++ 01_basics.js/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "33"
+// console.log(typeof score)
+
+let valueInno = Number(score)
+// console.log(typeof valueInno)
+
+// 33 = 33
+// 33abc = NaN
+// true = 1
+let num = 2
+let boolnum = Boolean(num)
+//console.log(boolnum)
+//*******************************************Operations*********************************************
+console.log("1"+2+2) //122
+console.log(1+2+"3") //33
+
+//=== is strict checking it also checks datatype
---
+++ 01_basics.js/datatype-summary.js
@@ -0,0 +1,4 @@
+// primitive
+// 7 types: String, number, boolean, null, undefined, symbol, bigint
+
+// Reference(non primitive): arrays, onjects, functions
