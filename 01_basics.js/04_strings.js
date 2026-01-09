0b10defa27a2e3f77936723172e22aa1b4208d57
Mansi2801-dev <saxenamansi804@gmail.com>
2026-01-09 15:52:56 +0000

    strings in js

---
+++ 01_basics.js/04_strings.js
@@ -0,0 +1,11 @@
+const name = "kai"
+const repoCount = 100
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+//this is an example of string interpolation
+
+const name2 = new String("cardan")
+console.log(name2[0])
+console.log(name2.__proto__)
+console.log(name2.length)
+console.log(name2.toUpperCase())
+console.log(name2.indexOf('a'))
