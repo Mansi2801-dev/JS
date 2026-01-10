62ebc0e3e4c0e965d5e4e238a8f7059608865d22
Mansi2801-dev <saxenamansi804@gmail.com>
2026-01-10 13:16:08 +0000

    maths and dates

---
+++ 01_basics.js/05_nums_and_maths.js
@@ -0,0 +1,13 @@
+const balance = 100
+//console.log(balance.toFixed(2))
+
+const num = 123.45678
+//console.log(num.toPrecision(5))
+
+const num2 = 1000000
+//console.log(num2.toLocaleString('en-IN')) // by default us standards
+//++++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++
+console.log(Math.random())// value b/w 0 and 1
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min+1)+min))
---
+++ 01_basics.js/06_date_and_time.js
@@ -0,0 +1,9 @@
+//Date
+let mydate = new Date()
+console.log(mydate)
+console.log(mydate.toString())
+console.log(mydate.toLocaleString())
+console.log(mydate.toDateString())
+
+let newdate = new Date(2026, 1, 10)//months start from 0
+console.log(newdate.toDateString())
