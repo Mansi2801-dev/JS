f4dc5e30e78d8f52dd29274c4d002d8978d58c62
Mansi2801-dev <saxenamansi804@gmail.com>
2026-01-10 13:30:03 +0000

    arrays

---
+++ 02_basics.js/01_arrays.js
@@ -0,0 +1,15 @@
+myarr = [1,2,3,4,5]
+// myarr.push(6)
+// console.log(myarr)
+// myarr.pop()
+// console.log(myarr)
+
+// in unshift parameter is given and value is added at first.
+// in shift no parameter is given and value is removed from first
+
+const newarr = myarr.join()
+console.log(newarr)
+console.log(myarr)
+console.log(typeof newarr)
+
+//in slice original array is not manipulated but in splice it is
