ec965b25d9099c0ce2a2a66cda31544656071565
Mansi2801-dev <saxenamansi804@gmail.com>
2026-01-11 12:08:22 +0000

    objects

--- 02_basics.js/02_objects.js
+++ 02_basics.js/02_objects.js
@@ -1,7 +1,9 @@
 //object literals
+const mysym = Symbol("key1")
 const jsuser = {
     name : 'Christian',
     "full name" : "Christian Harper",
+    [mysym]: "key1",
     age : 32,
     email : 'harperchristian32@user.com',
     location : 'Las Vegas'
@@ -9,3 +11,6 @@ const jsuser = {
 console.log(jsuser.name)//using dot operator, dont need to define the datatype
 console.log(jsuser['email'])//need to define datatype
 console.log(jsuser['full name'])
+console.log(jsuser[mysym])
+console.log(typeof mysym)
+//object.freeze() does not allow any further changes
